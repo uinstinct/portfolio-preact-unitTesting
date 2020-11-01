57856c769fa82c5a1ea880772def538cf5a1825e
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-11-02 02:04:33 +0530

    added meta tags for summary cards in Head added site urls in README

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Built with @[Next.JS](https://nextjs.org/)
 
+**Live on**
+
+[GitHub Pages](https://aditya-mitra.github.io/)
+
+[Netlify](https://aditya-mitra.netlify.app/)
+
+
+Deployment configured using *github actions*.
+
 ### Notes
 
 - All **content** that might appear to change are put in file named `constants.js`.
--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,19 @@ export default function IndexPage() {
                 <meta httpEquiv="content-type" content="text/html;charset=UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:creator" content="@Aditya_Mitra_" />
+                <meta name="twitter:title" content="Aditya Mitra" />
+                <meta name="twitter:description" content="The portfolio site of Aditya Mitra." />
+                <meta name="twitter:image" content="https://res.cloudinary.com/gamersinstinct7/image/upload/v1604254256/portfolio/fallbacks/png%20fallback.png" />
+                <meta name="twitter:image:alt" content="uinstinct" />
+
+                <meta property="og:title" content="Aditya Mitra" />
+                <meta property="og:site_name" content="Aditya Mitra" />
+                <meta property="og:url" content="https://aditya-mitra.netlify.app/" />
+                <meta property="og:description" content="The portfolio site of Aditya Mitra." />
+                <meta property="og:image" content="/me.jpg" />
+
                 {/* generics */}
                 <link rel="icon"
                     href="https://res.cloudinary.com/gamersinstinct7/image/upload/v1604255529/portfolio/favicons/32_fp8hpk.ico"
