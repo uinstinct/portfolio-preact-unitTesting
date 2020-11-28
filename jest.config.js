c62767a3ad5db5367d8b9205dc25515735b4abda
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-11-29 00:07:05 +0530

    jest setup in jest.config and test-utils

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+
+modules.exports = {
+    errorOnDeprecated: true,
+    moduleNameMapper: {
+        "^@/containers(.*)$": "<rootDir>/containers$1",
+        "^@/pages(.*)$": "<rootDir>/pages$1",
+        "^@/styles(.*)$": "<rootDir>/styles$1",
+    },
+    setupFilesAfterEnv: ["./tests/setupTests.js"],
+    testTimeout: 7000,
+    verbose: false,
+};
---
+++ tests/test-utils.js
@@ -0,0 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+const Provider = ({ children }) => {
+    return (
+        <ChakraProvider>
+            {children}
+        </ChakraProvider>
+    );
+}
+
+const customRender = (ui) => {
+    render(ui, { wrapper: Provider })
+}
+
+export * from "@testing-library/react";
+export { customRender as render }; // override
