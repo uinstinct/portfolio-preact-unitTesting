ba1793c3be3d60cdc13d6a2dd7a565d759446d99
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-11-28 19:09:01 +0530

    refactored components from cards put them in cardComponents.js

--- containers/Projects/card.js
+++ containers/Projects/card.js
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
 import { useColorMode, LightMode } from "@chakra-ui/react";
+
 import {
     handleDragStart, handleDrag, handleDragEnd,
     handleTouchStart, handleTouchEnd
-}
-    from "./cardMouseMovements";
+} from "./cardMouseMovements";
+import {
+    LinkButtons, StackTags, CardItems, RadioBullets
+} from './cardComponents';
 
 import {
     RadioGroup,
-    Icon, IconButton, Link,
-    Tag, TagLabel, TagRightIcon
 } from '@chakra-ui/react';
 
-import { allStyles, itemStyles, bulletStyles } from "@/styles/card";
-import { glossColour, glossIcon } from "./constants";
+import { allStyles } from "@/styles/card";
+
 
 export default function Card({ sourceItems, links, stacks }) {
 
     const { colorMode } = useColorMode();
+
     const uniqueMark = sourceItems[0].id;
     const [currentItem, setCurrentItem] = useState(uniqueMark);
     const totalItems = sourceItems.length;
@@ -31,7 +33,6 @@ export default function Card({ sourceItems, links, stacks }) {
     }
 
     useEffect(() => {
-        // this cannot be added in the displayCard function because this is a sideEffect
         const item = document.getElementById(`item${currentItem}`);
         item.classList.add("visible");
         if (totalItems > 1) {
@@ -40,8 +41,6 @@ export default function Card({ sourceItems, links, stacks }) {
         }
     }, [currentItem]);
 
-
-    // on initial component mount
     useEffect(() => {
         const item = document.getElementById(`item${uniqueMark}`);
         item.classList.add("visible");
@@ -51,80 +50,10 @@ export default function Card({ sourceItems, links, stacks }) {
         }
     }, []);
 
-
-    const items = sourceItems.map((sourceItem, idx) => {
-        const { id } = sourceItem;
-        return (
-            <div key={idx}>
-                <style jsx>
-                    {itemStyles}
-                </style>
-                <div className={`wrapper ${colorMode === "dark" ? "dark" : null}`}>
-                    <div key={idx} className={`source-item-${uniqueMark} item item-${idx + 1}`} id={`item${id}`}>
-                        <div className="imageContainer">
-                            <img
-                                src={sourceItem.imgSrc} alt={sourceItem.title}
-                                loading="lazy" height="300" width="300"
-                            />
-                        </div>
-                        <div className="content">
-                            <div className="title">{sourceItem.title}</div>
-                            <div className="description">{sourceItem.description}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    });
-
-    const bullets = totalItems > 1 ? sourceItems.map((sourceItem, idx) => {
-        const { id } = sourceItem;
-        return (
-            <div key={idx}>
-                <style jsx>
-                    {bulletStyles}
-                </style>
-                <label htmlFor={`tapInput${id}`} id={`tap${id}`} className="tap tap-${uniqueMark}">
-                    <input type="radio" id={`tapInput${id}`} value={id} checked={id === currentItem} onChange={displayCard} />
-                </label>
-            </div>
-        );
-    })
-        : null;
-
-    const linkButtons = links.map((link, idx) => {
-        const icon = <Icon as={glossIcon[link.provider]} />;
-        let colour = glossColour[link.provider];
-
-        if ('darkModeColour' in link && colorMode === "dark") {
-            colour = link.darkModeColour;
-        }
-
-        return (
-            <Link isExternal href={link.link} key={idx}>
-                <IconButton
-                    aria-label={link.provider}
-                    icon={icon} color={colour}
-                    variant="ghost" isRound="true" size="lg"
-                />
-            </Link>
-        );
-    })
-
-    const stackTags = stacks.map((stack, idx) => {
-        const icon = glossIcon[stack] || glossIcon["other"];
-        const colour = glossColour[stack] || "gray";
-
-        return (
-            <Tag
-                key={idx} colorScheme={colour}
-                rounded="full" variant="solid"
-            >
-                <TagLabel>{stack}</TagLabel>
-                <TagRightIcon as={icon} />
-            </Tag>
-        );
-    })
+    const Links = LinkButtons(links, colorMode);
+    const Stacks = StackTags(stacks);
+    const Items = CardItems(sourceItems, colorMode, uniqueMark);
+    const Bullets = RadioBullets(sourceItems, uniqueMark, currentItem, totalItems, displayCard);
 
     return (
         <>
@@ -151,22 +80,22 @@ export default function Card({ sourceItems, links, stacks }) {
                     )
                 }
             >
-                {items}
+                {Items}
                 <div className="bullets"
-                    style={{ top: `calc(100%/${items.length})` }}
+                    style={{ top: `calc(100%/${Items.length})` }}
                 >
                     <RadioGroup defaultValue="1" value={currentItem} >
                         <div className="sliders">
-                            {bullets}
+                            {Bullets}
                         </div>
                     </RadioGroup>
                 </div>
                 <LightMode>
                     <div className="links">
-                        {linkButtons}
+                        {Links}
                     </div>
                     <div className="stacks">
-                        {stackTags}
+                        {Stacks}
                     </div>
                 </LightMode>
             </div>
---
+++ containers/Projects/cardComponents.js
@@ -0,0 +1,98 @@
+import {
+    Icon, IconButton, Link,
+    Tag, TagLabel, TagRightIcon
+} from '@chakra-ui/react';
+
+import { glossColour, glossIcon } from "./constants";
+
+import { itemStyles, bulletStyles } from '@/styles/card';
+
+
+export const LinkButtons = (links, colorMode) =>
+    links.map((link, idx) => {
+        const icon = <Icon as={glossIcon[link.provider]} />;
+        let colour = glossColour[link.provider];
+
+        if ('darkModeColour' in link && colorMode === "dark") {
+            colour = link.darkModeColour;
+        }
+
+        return (
+            <Link isExternal href={link.link} key={idx}>
+                <IconButton
+                    aria-label={link.provider}
+                    icon={icon} color={colour}
+                    variant="ghost" isRound="true" size="lg"
+                />
+            </Link>
+        );
+    });
+
+export const StackTags = (stacks) =>
+    stacks.map((stack, idx) => {
+        const icon = glossIcon[stack] || glossIcon["other"];
+        const colour = glossColour[stack] || "gray";
+
+        return (
+            <Tag
+                key={idx} colorScheme={colour}
+                rounded="full" variant="solid"
+            >
+                <TagLabel>{stack}</TagLabel>
+                <TagRightIcon as={icon} />
+            </Tag>
+        );
+    });
+
+export const CardItems = (sourceItems, colorMode, uniqueMark) =>
+    sourceItems.map((sourceItem, idx) => {
+        const { id } = sourceItem;
+        return (
+            <div key={idx}>
+                <style jsx>
+                    {itemStyles}
+                </style>
+                <div className={`wrapper ${colorMode === "dark" ? "dark" : null}`}>
+                    <div
+                        key={idx} id={`item${id}`}
+                        className={`source-item-${uniqueMark} item item-${idx + 1}`}
+                    >
+                        <div className="imageContainer">
+                            <img
+                                src={sourceItem.imgSrc} alt={sourceItem.title}
+                                loading="lazy" height="300" width="300"
+                            />
+                        </div>
+                        <div className="content">
+                            <div className="title">{sourceItem.title}</div>
+                            <div className="description">{sourceItem.description}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    });
+
+export const RadioBullets =
+    (sourceItems, uniqueMark, currentItem, totalItems, displayCard) =>
+    totalItems > 1 ? sourceItems.map((sourceItem, idx) => {
+        const { id } = sourceItem;
+        return (
+            <div key={idx}>
+                <style jsx>
+                    {bulletStyles}
+                </style>
+                <label
+                    htmlFor={`tapInput${id}`} id={`tap${id}`}
+                    className="tap tap-${uniqueMark}"
+                >
+                    <input type="radio" id={`tapInput${id}`}
+                        value={id}
+                        checked={id === currentItem}
+                        onChange={displayCard}
+                    />
+                </label>
+            </div>
+        );
+    })
+        : null;
